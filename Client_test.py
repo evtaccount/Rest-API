35ff83c14f98b33c94b255fb3cef15acac2142f8
evgeny.evtushenko <4eshire.s@gmail.com>
2018-03-22 01:33:32 +0500

    Все работает!

--- Client_test.py
+++ Client_test.py
@@ -11,88 +11,104 @@ import Functions
 
 #Функция принимает заданные аршументы (незаданные остаются по дефолту) и формирует строку для передаяи по сети,
 #переводя в json и кодируя
-def send_request(id="", title="", text="", date_create=0000000000, date_update=0000000000):
-    send_data = {"id": id, "title": title, "text": text, "date_create": date_create, "date_update": date_update}
+def server_request(uuid_note="", title="", text="", date_create=0000000000, date_update=0000000000):
+    send_data = {"id": uuid_note, "title": title, "text": text, "date_create": date_create, "date_update": date_update}
     data = json.dumps(send_data).encode()
-    return data
+    sock.send(data)
+
+    recieved_data = sock.recv(1024)
+    return json.loads(recieved_data.decode("utf-8"))
 
 
-def get_notes_from_db():
-    exp_list = []
-    sock.send(send_request())
-    data = sock.recv(1024).decode()
-    data = json.loads(data)
+def print_all(data_to_print):
+    id_list = []
+    if len(data_to_print) == 0:
+        print("База пуста")
+    else:
+        for ind, readed_note in enumerate(data_to_print):
+            print("\n", ind + 1, ". Заголовок:      ", readed_note["title"])
+            print("     Текст заметки:  ", readed_note["text"])
+            print("     Дата создания:  ", time.ctime(int(readed_note["date_create"])))
+            print("     Дата изменения: ", time.ctime(int(readed_note["date_update"])))
+            id_list.append(readed_note["id"])
+    return id_list
 
-    for ind, readed_note in enumerate(data):
-        print(ind + 1, ". Заголовок:      ", readed_note["title"])
-        print("    Текст заметки:  ", readed_note["text"])
-        print("    Дата создания:  ", time.ctime(int(readed_note["date_create"])))
-        print("    Дата изменения: ", time.ctime(int(readed_note["date_update"])), "\n")
-        exp_list.append(readed_note["id"])
-    return exp_list
-
-
-note = {
-    "id": "",
-    "title": "",
-    "text": "",
-    "date_create": 0,
-    "date_update": 0
-}
-
-note_template = {
-    "id": "",
-    "title": "",
-    "text": "",
-    "date_create": 0,
-    "date_update": 0
-}
 
 sock = socket.socket()
 sock.connect(('localhost', 8080))
 print("Соединение установлено")
 
-while True:
-    print("""Выберите действие:
+choosen_mode = ''
+
+while choosen_mode != 'q':
+    choosen_mode = input("""
+Выберите действие
     1. Создать заметку
     2. Получить все заметки
     3. Удалить заметку
-    4. Редактировать заметку""")
-    choosen_mode = int(input())
+    4. Редактировать заметку
+    (Для завершения работы программы введите "q")
+    -> """)
 
-    if True:
+    if len(choosen_mode) == 1 and choosen_mode.isdigit():
         #Создание заметки
-        if choosen_mode == 1:
+        if choosen_mode == "1":
             title_input = ""
             text_input = ""
             while len(title_input) > 30 or len(title_input) == 0:
-                title_input = input("Введите заголовок заметки\nДлина заголовка должна быть не более 30 символов: ")
+                title_input = input("\nВведите заголовок заметки"
+                                    "\n(Длина заголовка должна быть не более 30 символов)\n -> ")
                 if len(title_input) > 30:
                     print("Ошибка ввода. Попробуйте ещё раз\n")
             while len(text_input) > 500 or len(text_input) == 0:
-                text_input = input("Введите текст заметки\nДлина заметки не должна превышать 500 символов")
+                text_input = input("\nВведите текст заметки"
+                                   "\n(Длина заметки не должна превышать 500 символов)\n -> ")
                 if len(text_input) > 500:
                     print("Ошибка ввода. Попробуйте ещё раз\n")
 
-            sock.send(send_request(title=title_input, text=text_input))
-            print("Заметка успешно создана")
+            answer = server_request(title=title_input, text=text_input)
+            if answer["title"] == title_input and answer["text"] == text_input and answer["id"] != "":
+                print("\nЗаметка успешно создана")
+            else:
+                print("Ошибка записи")
 
-        elif choosen_mode == 2:
-            get_notes_from_db()
+        elif choosen_mode == "2":
+            print_all(server_request())
 
-        elif choosen_mode == 3:
-            id_list = get_notes_from_db()
-            select_note = int(input("Укажите номер заметки для удаления - "))
-            sock.send(send_request(id=id_list[select_note-1]))
+        elif choosen_mode == "3":
+            id_list = print_all(server_request())
+            select_note = int(input("\nУкажите номер заметки для удаления -> "))
+            while not select_note in range(len(id_list) + 1):
+                print("Введенный номер выходит за границы диапозона")
+                select_note = int(input("\nУкажите номер заметки для удаления -> "))
+            answer = server_request(uuid_note=id_list[select_note-1])
+            if answer["id"] == id_list[select_note-1]:
+                print("Удаление прошло успешно")
 
-        elif choosen_mode == 4:
-            id_list = get_notes_from_db()
-            print(id_list)
-            select_note = int(input("Укажите номер заметки для редактирования - "))
-            new_title = input("Введите новый заголовок")
-            new_text = input("Введите новую заметку")
-            print("id_list: ", id_list)
-            sock.send(send_request(id=id_list[select_note-1], title=new_title, text=new_text))
+        elif choosen_mode == "4":
+            id_list = print_all(server_request())
+            select_note = int(input("\nУкажите номер заметки для редактирования -> "))
+            while not select_note in range(len(id_list) + 1):
+                print("Введенный номер выходит за границы диапозона")
+                select_note = int(input("\nУкажите номер заметки для редактирования -> "))
+
+            new_title = ''
+            new_text = ''
+            while len(new_title) > 30 or len(new_title) == 0:
+                new_title = input("\nВведите заголовок заметки"
+                                    "\n(Длина заголовка должна быть не более 30 символов)\n -> ")
+                if len(new_title) > 30:
+                    print("Ошибка ввода. Попробуйте ещё раз\n")
+            while len(new_text) > 500 or len(new_text) == 0:
+                new_text = input("\nВведите текст заметки"
+                                   "\n(Длина заметки не должна превышать 500 символов)\n -> ")
+                if len(new_text) > 500:
+                    print("Ошибка ввода. Попробуйте ещё раз\n")
+            answer = server_request(uuid_note=id_list[select_note-1], title=new_title, text=new_text)
+            if answer["title"] == new_title and answer["text"] == new_text:
+                print("Заметка успешно отредактирована")
+            else:
+                print("Новая заметка не сохранена")
         else:
             pass
     else:
--- Server_test.py
+++ Server_test.py
@@ -10,21 +10,14 @@ import Classes
 import Functions
 
 
-note_template = {
-    "id": "",
-    "title": "",
-    "text": "",
-    "date_create": 0,
-    "date_update": 0
-}
+keys = ["id", "title", "text", "date_create", "date_update"]
 
 
-#Функция принимает заданные аршументы (незаданные остаются по дефолту) и формирует строку для передаяи по сети,
-#переводя в json и кодируя
-def send_note(id="", title="", text="", date_create=0000000000, date_update=0000000000):
-    send_data = {"id": id, "title": title, "text": text, "date_create": date_create, "date_update": date_update}
-    data = json.dumps(send_data).encode()
-    return data
+# Данная фенкция формирует ответ клиенту в формате словаря с необходимыми ключами
+def send_answer(uuid_note="", title="", text="", date_create=0000000000, date_update=0000000000):
+    send_data = {"id": uuid_note, "title": title, "text": text, "date_create": date_create, "date_update": date_update}
+    tmp_data = json.dumps(send_data).encode()
+    return tmp_data
 
 
 #Функция считывает из файла все заметки и возвращает список словарей
@@ -34,7 +27,7 @@ def get_notes():
     with open("notes.txt", encoding='utf-8') as f:
         for note in f:
             note = note.strip().split(',')
-            for n, item in enumerate(note_template.keys()):
+            for n, item in enumerate(keys):
                 current_note[item] = note[n]
             notes.append(current_note.copy())
     print(notes)
@@ -44,6 +37,11 @@ def get_notes():
 #Функция записывает новую заметку в файл. В случае успеха возвращает True; если запись не выполнена - возвращает False
 def save_note(new_note):
     tmp = []
+    saved_note = []
+    new_note["id"] = str(uuid.uuid4())
+    new_note["date_create"] = int(time.time())
+    new_note["date_update"] = int(time.time())
+
     for key in new_note:
         tmp.append(str(new_note[key]))
     tmp = ','.join(tmp)
@@ -52,36 +50,41 @@ def save_note(new_note):
         with open("notes.txt", "a", encoding='utf-8') as f:
             f.write(tmp + '\n')
             print("Заметка успешно создана")
-            return True
+            saved_note = tmp.strip().split(",")
     except:
         print("Ошибка записи")
-        return False
+    finally:
+        return saved_note
 
 
 #Функция принимает в качестве аргументов список словарей и id нужного словаря. Словарь с этим id удаляет из списка.
 def del_note(selected_id):
     readed_notes = get_notes()
-    for ind, note in enumerate(readed_notes):
-        if note["id"] == selected_id:
-            deleted_note = readed_notes.pop(ind)
+    deleted_note = []
+    for ind, tmp_note in enumerate(readed_notes):
+        if tmp_note["id"] == selected_id:
+            tmp_note = readed_notes.pop(ind)
             save_all(readed_notes)
             break
+    for key in tmp_note.keys():
+        deleted_note.append(tmp_note[key])
     return deleted_note
 
 
 #Функция принимает в качестве аргументов список словарей и отредактированный словарь. Заменяет в списке в искомом
 #словаре "title" и "text" на новые. При этом обновляется "date_update". Возвращается отредактированный список словарей.
-def replace_not(new_note):
+def replace_note(new_note):
+    replaced_note = []
     readed_notes = get_notes()
     for note in readed_notes:
         if new_note["id"] == note["id"]:
             note["title"] = new_note["title"]
             note["text"] = new_note["text"]
             note["date_update"] = str(int(time.time()))
+            replaced_note = note
             break
-    print("Note list: ", readed_notes)
     save_all(readed_notes)
-    return readed_notes
+    return replaced_note
 
 
 #Принимает в качестве аргумента список словарей и записывает из в файл
@@ -102,28 +105,26 @@ print("connected:", addr)
 
 while True:
     data = conn.recv(1024)
-    recieved_date = json.loads(data.decode("utf-8"))
+    recieved_data = json.loads(data.decode("utf-8"))
 
     #Добавить заметку
-    if recieved_date["id"] == "" and recieved_date["title"] != "":
-        recieved_date["id"] = str(uuid.uuid4())
-        recieved_date["date_create"] = int(time.time())
-        recieved_date["date_update"] = int(time.time())
-        save_note(recieved_date)
+    if recieved_data["id"] == "" and recieved_data["title"] != "":
+        note = save_note(recieved_data)
+        conn.sendall(send_answer(*note))
 
     # Показать имеющиеся заметки
-    if recieved_date["id"] == "" and recieved_date["title"] == "":
+    elif recieved_data["id"] == "" and recieved_data["title"] == "":
         notes = get_notes()
-        data = json.dumps(notes).encode()
-        conn.sendall(data)
+        answer = json.dumps(notes).encode()
+        conn.sendall(answer)
 
     # Удалить заметку
-    if recieved_date["id"] != "" and recieved_date["title"] == "":
-        print("Here is ok")
-        del_note(recieved_date["id"])
+    elif recieved_data["id"] != "" and recieved_data["title"] == "":
+        note = del_note(recieved_data["id"])
+        print("note", note)
+        conn.sendall(send_answer(*note))
 
     # Изменить заметку
-    if recieved_date["id"] != "" and recieved_date["title"] != "" and recieved_date["text"] != "":
-        print("recieved date - ", recieved_date)
-        print("Edit here")
-        print("replaced note list", replace_not(new_note=recieved_date))
+    elif recieved_data["id"] != "" and recieved_data["title"] != "" and recieved_data["text"] != "":
+        note = replace_note(new_note=recieved_data)
+        conn.sendall(send_answer(*note))
